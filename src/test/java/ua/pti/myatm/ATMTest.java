5c9376124494a01e2845fe6b195be41f7fde66d5
folmas <folmas@mail.ru>
2015-12-21 15:10:17 +0200

    Update ATMTest.java

--- src/test/java/ua/pti/myatm/ATMTest.java
+++ src/test/java/ua/pti/myatm/ATMTest.java
@@ -44,13 +44,13 @@ public class ATMTest {
 
     @Test
     public void testValidateBlockedCardValidPin() {
+        System.out.println("with blocked card");
+        boolean expResult = false;
         Card card = mock(Card.class);
         int pinCode = 3210;
+        ATM instance = new ATM(30);
         when(card.checkPin(pinCode)).thenReturn(true);
         when(card.isBlocked()).thenReturn(true);
-        System.out.println("with blocked card");
-        ATM instance = new ATM(30);
-        boolean expResult = false;
         boolean result = instance.validateCard(card, pinCode);
         assertEquals(expResult, result);
     }
@@ -60,10 +60,10 @@ public class ATMTest {
         System.out.println("validateCard no block, true pin");
         Card card = mock(Card.class);
         int pinCode = 3210;
-        when(card.checkPin(pinCode)).thenReturn(true);
-        when(card.isBlocked()).thenReturn(false);
         ATM instance = new ATM(30);
         boolean expResult = true;
+        when(card.checkPin(pinCode)).thenReturn(true);
+        when(card.isBlocked()).thenReturn(false);
         boolean result = instance.validateCard(card, pinCode);
         assertEquals(expResult, result);
     }
@@ -71,11 +71,11 @@ public class ATMTest {
     @Test
     public void testValidateCardUnblockedCardInvalidPin() {
         System.out.println("validateCard with wrong pin");
+        ATM instance = new ATM(30);
+        boolean expResult = false;
         Card card = mock(Card.class);
         when(card.checkPin(anyInt())).thenReturn(false);
         when(card.isBlocked()).thenReturn(false);
-        ATM instance = new ATM(30);
-        boolean expResult = false;
         boolean result = instance.validateCard(card, 3210);
         assertEquals(expResult, result);
     }
@@ -94,7 +94,6 @@ public class ATMTest {
         when(card.getAccount()).thenReturn(account);
         when(card.checkPin(anyInt())).thenReturn(true);
         when(card.isBlocked()).thenReturn(false);
-
         double startBalanceInATM = 100;
         ATM instance = new ATM(startBalanceInATM);
         instance.validateCard(card,3210);
@@ -114,6 +113,7 @@ public class ATMTest {
     @Test
     public void testGetCash() {
         System.out.println("get Cash allright");
+        double expResult = accountBalanceAfter;
         double accountBalanceBefore = 1000;
         double amountForWithdraw = 100;
         double accountBalanceAfter = 900;
@@ -126,7 +126,6 @@ public class ATMTest {
         when(card.isBlocked()).thenReturn(false);
         ATM instance = new ATM(1000);
         instance.validateCard(card,3210);
-        double expResult = accountBalanceAfter;
         double result = instance.getCash(amountForWithdraw);
         assertEquals(expResult, result, 0);
     }
@@ -158,9 +157,8 @@ public class ATMTest {
        double accountWithdraw = 1;
         double ATMBalance = 200;
         ATM instance = new ATM(ATMBalance);
-         System.out.println("getCash with Empty ATM");
+        System.out.println("getCash with Empty ATM");
         double result = instance.getCash(accountWithdraw);
-    
     }
     
 
